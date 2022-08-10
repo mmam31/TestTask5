715da49b8d8fad3118a3bc268996548b33b9ffd7
Modris <modris.martinovs@gmail.com>
2022-08-10 11:03:52 +0300

    Initial commit

---
+++ app/src/main/java/com/eu/modris/SportsList.java
@@ -0,0 +1,14 @@
+package com.eu.modris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SportsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sports_list);
+    }
+}
