99e4ab90adf9bbb29c9d5f6297b4c22b43363ef8
Modris <modris.martinovs@gmail.com>
2022-08-10 18:01:24 +0300

    update list of sports6

--- app/src/main/java/com/eu/modris/MainActivity.java
+++ app/src/main/java/com/eu/modris/MainActivity.java
@@ -2,13 +2,34 @@ package com.eu.modris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSportsList();
+
+            }
+        });
+
+    }
+
+    public void openSportsList(){
+        Intent intent = new Intent(this, SportsList.class);
+        startActivity(intent);
     }
 }
